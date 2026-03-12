2c16d37705c7838ef14bf694bc420777870c6ab3
Mehmet Özay <mehmetozay45@hotmail.com>
2026-03-13 01:44:51 +0300

    adaptive sort yapısı eklendi
    Stack'e hangi operasyon ile push yaptığı tespit edildi

---
+++ srcs/sort_adaptive/ft_adaptive_sort.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_adaptive_sort.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mozay <mozay@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/13 04:00:00 by mozay             #+#    #+#             */
+/*   Updated: 2026/03/13 00:40:05 by mozay            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../push_swap.h"
+
+void	ft_adaptive_sort(t_stack **a, t_stack **b, t_bench *bench)
+{
+	double	disorder;
+
+	disorder = ft_compute_disorder(*a);
+	if (disorder < 0.2)
+	{
+		bench->complexity = "O(n²)";
+		ft_bubble_sort(a, b, bench);
+	}
+	else if (disorder < 0.5)
+	{
+		bench->complexity = "O(n√n)";
+		ft_block_sort(a, b, bench);
+	}
+	else
+	{
+		bench->complexity = "O(n log n)";
+		ft_heap_sort(a, b, bench);
+	}
+}
---
+++ srcs/sort_adaptive/ft_adaptive_utils.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_adaptive_utils.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mozay <mozay@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/13 04:00:00 by mozay             #+#    #+#             */
+/*   Updated: 2026/03/13 00:39:56 by mozay            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../push_swap.h"
+
+static void	ft_dispatch_simple(t_stack **a, t_stack **b, t_bench *bench)
+{
+	bench->strategy = "Bubble Sort";
+	bench->complexity = "O(n²)";
+	ft_bubble_sort(a, b, bench);
+}
+
+static void	ft_dispatch_medium(t_stack **a, t_stack **b, t_bench *bench)
+{
+	bench->strategy = "Block Sort";
+	bench->complexity = "O(n√n)";
+	ft_block_sort(a, b, bench);
+}
+
+static void	ft_dispatch_complex(t_stack **a, t_stack **b, t_bench *bench)
+{
+	bench->strategy = "Heap Sort";
+	bench->complexity = "O(n log n)";
+	ft_heap_sort(a, b, bench);
+}
+
+void	ft_dispatch_sort(t_stack **a, t_stack **b, char *strat, t_bench *bch)
+{
+	if (ft_strcmp(strat, "simple") == 0)
+		ft_dispatch_simple(a, b, bch);
+	else if (ft_strcmp(strat, "medium") == 0)
+		ft_dispatch_medium(a, b, bch);
+	else if (ft_strcmp(strat, "complex") == 0)
+		ft_dispatch_complex(a, b, bch);
+	else
+		ft_adaptive_sort(a, b, bch);
+}
+
+void	ft_sort_stacks(t_stack **a, t_stack **b, char *strat, t_bench *bch)
+{
+	int	size;
+
+	if (!a || !*a)
+		return ;
+	size = ft_stack_size(*a);
+	if (size <= 1 || ft_is_sorted(*a))
+		return ;
+	if (size == 2)
+	{
+		ft_sort_two(a, bch);
+		return ;
+	}
+	if (size <= 5)
+	{
+		ft_simple_sort(a, b, bch);
+		return ;
+	}
+	ft_dispatch_sort(a, b, strat, bch);
+}
