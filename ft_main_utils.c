f085e42a7ff86ff9db1d424ffbcba3a847411012
Mehmet Özay <mehmetozay45@hotmail.com>
2026-03-30 00:23:17 +0300

    stack argümanlarının kontrolleri ve flag kontrollerine bakıldı

--- ft_main_utils.c
+++ ft_main_utils.c
@@ -6,38 +6,25 @@
 /*   By: mozay <mozay@student.42kocaeli.com.tr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/13 02:30:00 by mozay             #+#    #+#             */
-/*   Updated: 2026/03/29 21:02:04 by mozay            ###   ########.fr       */
+/*   Updated: 2026/03/29 23:42:14 by mozay            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_stack	*ft_create_node(long value)
+int	ft_is_flag(char *arg)
 {
-	t_stack	*new;
-
-	new = (t_stack *)malloc(sizeof(t_stack));
-	if (!new)
-		return (NULL);
-	new->value = (int)value;
-	new->index = 0;
-	new->next = NULL;
-	return (new);
-}
-
-void	ft_add_node(t_stack **stack, t_stack *new)
-{
-	t_stack	*last;
-
-	if (!*stack)
-		*stack = new;
-	else
-	{
-		last = *stack;
-		while (last->next)
-			last = last->next;
-		last->next = new;
-	}
+	if (ft_strcmp(arg, "--bench") == 0)
+		return (1);
+	if (ft_strcmp(arg, "--simple") == 0)
+		return (1);
+	if (ft_strcmp(arg, "--medium") == 0)
+		return (1);
+	if (ft_strcmp(arg, "--complex") == 0)
+		return (1);
+	if (ft_strcmp(arg, "--adaptive") == 0)
+		return (1);
+	return (0);
 }
 
 void	ft_assign_indices(t_stack *stack)
